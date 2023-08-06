014f5655907525c05d104b5ff4798b6c2be59786
JIN <parksonice@naver.com>
2023-08-06 17:04:46 +0900

    [Feat] train.py
    - 병렬 처리를 위해서 파일 10개로 분할

---
+++ Train/train40.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from supervised.automl import AutoML
+
+df = pd.read_csv('../data_file/fix_holiday_feature_train.csv')
+
+for i in range(41, 51):
+    building_df = df[df['building_number'] == i].copy()
+    building_df.drop(['building_number','date_time', 'building_type', 'total_area', 'cooling_area', 'upper_outliers', 'lower_outliers', 'outliers', 'anomaly', 'solar_power_capacity'], axis=1 , inplace=True)
+    X = building_df.drop('power_consumption', axis=1)
+    y = building_df.power_consumption
+    automl = AutoML(mode = 'Optuna',
+                    algorithms=["CatBoost", "Xgboost", "LightGBM"],
+                    results_path=f"building_train{i}",
+                    optuna_time_budget=3600)
+    automl.fit(X, y)
